4af8e0990e0a59578aace8e37f7427d966bd1e39
liuseen-l <773890440@qq.com>
2023-07-18 17:12:11 +0800

    feat: init

---
+++ .eslintrc.cjs
@@ -0,0 +1,12 @@
+module.exports ={
+  "extends": [
+    "@antfu",
+    "@unocss"
+  ],
+  "rules": {
+    "vue/no-v-text-v-html-on-component": "off",
+    "no-console": "off",
+    "@typescript-eslint/ban-types": "off",
+    "n/prefer-global/process": "off"
+  }
+}
